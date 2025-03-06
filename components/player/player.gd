d5539781d18c5df1d4bbdadb6ef48fc9d957b8e4
Tibo <goiffonthibaud@protonmail.com>
2025-03-06 11:03:55 +0100

    update: character control code

--- components/player/player.gd
+++ components/player/player.gd
@@ -2,7 +2,7 @@ extends RigidBody3D
 
 @export var jump_height : float = 2.5
 @export var jump_time_to_peak : float = 0.4
-@export var jump_time_to_descent : float = 0.3
+@export var jump_time_to_descent : float = 0.35
 
 @onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
 @onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
@@ -28,7 +28,11 @@ var last_movement_input : Vector2 = Vector2.ZERO
 
 var ragdoll : bool = false : set = _set_ragdoll
 
-var was_on_floor : bool = false
+var _is_on_floor : bool = false
+var _was_on_floor : bool = false
+
+# The “_integrate_forces” method is a quick translation of the integration of the character's body movements.
+# This code is not optimal; perhaps “move_and_collide” should be used to check is_on_floor.
 
 func _set_ragdoll(value : bool) -> void:
 	ragdoll = value
@@ -44,9 +48,11 @@ func _ready():
 		)
 
 func _unhandled_input(event):
-	if event.is_action_pressed("attack"):
+	if event.is_action_pressed("ragdoll"):
 		ragdoll = !ragdoll
+
 	if (event.is_action_pressed("wave")
+		&& _is_on_floor
 		&& !godot_plush_skin.is_waving()):
 		godot_plush_skin.wave()
 
@@ -54,14 +60,13 @@ func _integrate_forces(state : PhysicsDirectBodyState3D):
 	if ragdoll: return
 	var camera : Camera3D = get_viewport().get_camera_3d()
 	if camera == null: return
+	var is_waving : bool = godot_plush_skin.is_waving()
 	movement_input = Input.get_vector("left", "right", "up", "down").rotated(-camera.global_rotation.y)
-	var is_running : bool = Input.is_action_pressed("run") && !godot_plush_skin.is_waving()
+	var is_running : bool = Input.is_action_pressed("run")
 	var vel_2d = Vector2(state.linear_velocity.x, state.linear_velocity.z)
-	var is_moving : bool = movement_input != Vector2.ZERO
-	
-	physics_material_override.friction = 0.0 if is_moving else 0.8
+	var is_moving : bool = movement_input != Vector2.ZERO && !is_waving
 
-	if is_moving && !godot_plush_skin.is_waving():
+	if is_moving:
 		godot_plush_skin.set_state("run" if is_running else "walk")
 		var speed = run_speed if is_running else base_speed
 		vel_2d += movement_input * speed * 8.0 * state.step
@@ -75,36 +80,45 @@ func _integrate_forces(state : PhysicsDirectBodyState3D):
 	visual_root.rotation.y = rotate_toward(visual_root.rotation.y, target_angle, 6.0 * state.step)
 	var angle_diff = angle_difference(visual_root.rotation.y, target_angle)
 	godot_plush_skin.tilt = move_toward(godot_plush_skin.tilt, angle_diff, 2.0 * state.step)
-	
-	var is_on_floor : bool = _is_on_floor(state)
-	
-	movement_dust.emitting = is_running && is_on_floor && movement_input != Vector2.ZERO
-	
-	if is_on_floor:
-		if Input.is_action_just_pressed("jump") && !godot_plush_skin.is_waving():
+
+	_is_on_floor = _get_is_on_floor(state)
+
+	movement_dust.emitting = is_moving && is_running && _is_on_floor
+
+	# Check jump and fall 
+	if _is_on_floor:
+		if Input.is_action_just_pressed("jump") && !is_waving:
 			godot_plush_skin.set_state("jump")
 			state.linear_velocity.y = -jump_velocity
-			
+
 			var jump_particles = JUMP_PARTICLES_SCENE.instantiate()
 			add_sibling(jump_particles)
 			jump_particles.global_transform = global_transform
-			
+
 			do_squash_and_stretch(1.2, 0.1)
 	else:
 		godot_plush_skin.set_state("fall")
-		
-	var gravity = jump_gravity if state.linear_velocity.y > 0.0 else fall_gravity
 
+	# Add ground friction
+	physics_material_override.friction = 0.0 if is_moving else 0.8
+
+	# Add air damp when not moving
+	if !_is_on_floor && !is_moving:
+		vel_2d = vel_2d.move_toward(Vector2.ZERO, base_speed * state.step)
+		linear_velocity.x = vel_2d.x
+		linear_velocity.z = vel_2d.y
+
+	# Add gravity
+	var gravity = jump_gravity if state.linear_velocity.y > 0.0 else fall_gravity
 	state.linear_velocity.y -= gravity * state.step
-	
 	state.linear_velocity = state.linear_velocity.limit_length(fall_gravity)
-	
-	if !was_on_floor && is_on_floor:
+
+	# Add ground collision feedback
+	if !_was_on_floor && _is_on_floor:
 		_on_hit_floor(state.linear_velocity.y)
+	_was_on_floor = _is_on_floor
 	
-	was_on_floor = is_on_floor
-	
-func _is_on_floor(state : PhysicsDirectBodyState3D) -> bool:
+func _get_is_on_floor(state : PhysicsDirectBodyState3D) -> bool:
 	for col_idx in state.get_contact_count():
 		var col_normal = state.get_contact_local_normal(col_idx)
 		return col_normal.dot(Vector3.UP) > -0.5
